3b72fd2446c8ba24f5e7573f5d8727fabec0c0d5
anami823 <anami823@gmail.com>
2019-07-31 09:44:23 -0700

    Add boiler plate css/html/js files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*----- constants -----*/ 
+/*----- app's state (variables) -----*/ 
+/*----- cached element references -----*/ 
+/*----- event listeners -----*/ 
+/*----- functions -----*/
+
+$('.big-and-round')
+
+var $liArr = $('li')
+
+var $last = $liArr[$liArr.length - 1]
+
